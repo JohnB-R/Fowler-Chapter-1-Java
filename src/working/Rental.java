227a52e2cee74a7678dfab405015e46c5d644755
John Boyd-Rainey <john@boyd-rainey.de>
2022-12-08 12:32:59 +0100

    First commit

---
+++ src/working/Rental.java
@@ -0,0 +1,25 @@
+package working;//From book: 'Refactoring' by Martin Fowler
+//This is the original code before refactoring begins
+
+/**
+ * The rental class represents a customer renting a movie.
+ */
+public class Rental {
+
+	private final Movie movie;
+	private final int daysRented;
+	
+	public Rental(Movie movie, int daysRented) {
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+}
